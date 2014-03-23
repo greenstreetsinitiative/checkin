35b5e6681aff746a86b0136623154cb9625d74cf
Christian Spanring <cspanring@gmail.com>
2014-03-23 16:18:24 -0400

    updates multiple legs function in form
    adds first prototype in walk/ride day section

---
+++ survey/static/js/form.js
@@ -0,0 +1,26 @@
+$(function() {
+  // add another leg
+  $('.btn.morelegs').on('click', function(event) {
+    var targetLegs = $(event.target).data('target'),
+    $targetLegs = $('#' + targetLegs),
+    $lastLeg = $('.leg:last', $targetLegs),
+    $removeLegBtn = $('<button class="btn btn-danger"><span class="button">X</span></button>'),
+    $newLeg;
+
+    event.preventDefault();
+
+    $newLeg = $lastLeg
+    .clone()
+    .appendTo($targetLegs);
+
+    $removeLegBtn.on('click', function(event) {
+      var $leg = $(event.target).parentsUntil($targetLegs);
+      event.preventDefault();
+      $leg.remove();
+    });
+
+    $('div:first-child', $newLeg)
+    .addClass('right')
+    .html($removeLegBtn);
+  });
+});
