22176ab7ce9f6bd3316b833eb9d62ecf0aa3e77b
Christian Spanring <cspanring@gmail.com>
2014-04-15 08:57:37 -0400

    implements confirmation email

--- survey/views.py
+++ survey/views.py
@@ -1,13 +1,15 @@
+from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.forms.models import inlineformset_factory
 
-import json
-
-from survey.models import Commutersurvey, Employer, Leg
+from survey.models import Commutersurvey, Employer, Leg, Month
 from survey.forms import CommuterForm
+
+import json
+import mandrill
 
 
 def process_request(request):
@@ -67,6 +69,13 @@ def update_existing_survey(survey, survey_id, created, legs):
         leg.delete()
     return survey
 
+def send_email(template, template_content, message):
+    try:
+        mandrill_client = mandrill.Mandrill(settings.MANDRILL_API_KEY)
+        mandrill_client.messages.send_template(template_name=template, template_content=template_content, message=message)
+    except mandrill.Error, e:
+        pass
+
 def api(request):
     """
     Simple REST api for survey data
@@ -83,23 +92,60 @@ def api(request):
         except KeyError:
             legs = []
         
+        try:
+            data['wr_day_month'] = Month.objects.get(pk=data['wr_day_month'])
+        except Month.DoesNotExist:
+            return HttpResponse('No such Walk/Ride Day.', status=500)
+
         survey = Commutersurvey(**data)
         
         # check for existing survey
         try:
-            existing_survey = Commutersurvey.objects.get(month=data['month'], email=data['email'])
+            existing_survey = Commutersurvey.objects.get(wr_day_month=data['wr_day_month'], email=data['email'])
             update_existing_survey(survey, existing_survey.id, existing_survey.created, legs)
         except ObjectDoesNotExist:
             pass
         except MultipleObjectsReturned:
             # cleanup, keep the most recent one and delete the rest
-            surveys = Commutersurvey.objects.filter(month=data['month'], email=data['email']).order_by('-created')
+            surveys = Commutersurvey.objects.filter(wr_day_month=data['wr_day_month'], email=data['email']).order_by('-created')
             for s in surveys[1:]:
                 s.delete()
             update_existing_survey(survey, surveys[0].id, surveys[0].created, legs)
 
-        survey.save_with_legs(legs=legs)
-        return HttpResponse(status=200)
+        try:
+            survey.save_with_legs(legs=legs)
+            
+            try:
+                next_wr_day = 'Remember to check-in for next month\'s Walk/Ride Day on %s.' % Month.objects.filter(wr_day__gt=survey.wr_day_month.wr_day)[0].wr_day_humanized
+            except IndexError:
+                next_wr_day = 'Remember to check-in again next season.'
+            
+            template_content = [{
+                'content': survey.name or survey.email, 
+                'name': 'salutation'
+            }, {
+                'content': survey.wr_day_month.month,
+                'name': 'current_wr_day'
+            }, {
+                'content': next_wr_day,
+                'name': 'next_wr_day'
+            }]
+            message = {
+                'from_email': 'checkin@gogreenstreets.org',
+                'from_name': 'Green Streets Initiative',
+                'metadata': {'website': 'checkin.gogreenstreets.org'},
+                'subject': 'Walk/Ride Day %s Checkin' % survey.wr_day_month.month,
+                'to': [
+                    {'email': survey.email, 
+                    'name': survey.name or survey.email, 
+                    'type': 'to'}
+                ],
+                'track_opens': True
+            }
+            send_email('checkin-confirmation', template_content, message)
+            return HttpResponse(status=200)
+        except:
+            return HttpResponse(status=500)
 
-    return HttpResponse(status=500)
+    return HttpResponse(status=403)
 
