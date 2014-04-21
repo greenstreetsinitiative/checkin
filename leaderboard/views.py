dfd3e34d8279d7421a19313640d3b26f4a9ec0b5
John Freeman <johnf098@comcast.net>
2014-04-20 22:20:50 -0400

    fixed survey query to work with new months, subteams

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -120,7 +120,7 @@ def new_leaderboard(request, empid=0, filter_by='sector', _filter=0, sort='parti
     if empid != 0:
         context['chart'] = json.dumps(getCanvasJSChart(emp) )
         emp_stats = getBreakDown(emp, month)
-        nsurveys = emp.get_surveys(month=month).count()
+        nsurveys = len(get_lb_surveys(emp, month) )
         try: 
             context['participation'] = ( float(nsurveys) / (float(emp.nr_employees)*nmonths) ) * 100
         except TypeError, ZeroDivisionError:
@@ -343,12 +343,17 @@ surveys_cache = {}
 def get_lb_surveys(emp, month):
     global surveys_cache
     surveys = []
+
+    sectorEmps = Employer.objects.filter(sector=EmplSector.objects.get(parent=emp.name))
     if emp.name not in surveys_cache:
-        surveys_cache[emp.name] = Commutersurvey.objects.prefetch_related("leg_set").filter(employer=emp, month__in=Month.objects.active_months_list() )
+        if emp.is_parent:
+            surveys_cache[emp.name] = Commutersurvey.objects.prefetch_related("leg_set").filter(employer__in=sectorEmps, wr_day_month__in=Month.objects.filter(active='t') )
+        else:
+            surveys_cache[emp.name] = Commutersurvey.objects.prefetch_related("leg_set").filter(employer=emp, wr_day_month__in=Month.objects.filter(active='t') )
     if month == 'all':
         return surveys_cache[emp.name]
     for survey in surveys_cache[emp.name]:
-        if survey.month == month:
+        if survey.wr_day_month.month == month:
             surveys.append(survey)
     return surveys
 
